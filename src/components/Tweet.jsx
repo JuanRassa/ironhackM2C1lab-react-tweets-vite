39a19493521a7d58e9647f003120aba8d7a4b973
Juan Rassa <jmrassa.dev@gmail.com>
2023-11-18 16:47:51 +0000

    Complete iteration 2: Replace the static data in Tweet object for data passed by props.

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,37 +1,38 @@
-function Tweet() {
+function Tweet(props) {
+  const {
+    tweet: {
+      message,
+      timestamp,
+      user: { name, image, handle },
+    },
+  } = props;
+  console.log(name);
   return (
-    <div className="tweet">
-      <img
-        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
-        className="profile"
-        alt="profile"
-      />
+    <div className='tweet'>
+      <img src={image} className='profile' alt='profile' />
 
-      <div className="body">
-        <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
+      <div className='body'>
+        <div className='top'>
+          <span className='user'>
+            <span className='name'>{name}</span>
+            <span className='handle'>{handle}</span>
           </span>
 
-          <span className="timestamp">Nov 30, 2020</span>
+          <span className='timestamp'>{timestamp}</span>
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
+        <p className='message'>{message}</p>
 
-        <div className="actions">
+        <div className='actions'>
           {/* Font Awesome icons */}
-          <i className="far fa-comment" data-testid="comment-icon"></i>
-          <i className="fas fa-retweet" data-testid="retweet-icon"></i>
-          <i className="far fa-heart" data-testid="heart-icon"></i>
-          <i className="fas fa-share" data-testid="share-icon"></i>
+          <i className='far fa-comment' data-testid='comment-icon'></i>
+          <i className='fas fa-retweet' data-testid='retweet-icon'></i>
+          <i className='far fa-heart' data-testid='heart-icon'></i>
+          <i className='fas fa-share' data-testid='share-icon'></i>
         </div>
       </div>
 
-      <i className="fas fa-ellipsis-h"></i>
+      <i className='fas fa-ellipsis-h'></i>
     </div>
   );
 }
